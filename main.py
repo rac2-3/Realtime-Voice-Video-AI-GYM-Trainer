095de97f28f09301801b07ef5d9626ec5c0eb030
rac2-3 <150582020+rac2-3@users.noreply.github.com>
2026-08-26 17:56:38 +0530

    Complete add styles and fonts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
-
+import os
 
 
 from services.auth.login_wall import render_login_wall
 from services.state.session_defaults import initial_session_defaults
 from services.config.workout_config import EXERCISE_OPTIONS
+from services.ui.style_loader import load_css, inject_local_font
 
 def main():
     st.set_page_config(
@@ -13,6 +14,9 @@ def main():
         initial_sidebar_state="expanded",
         layout="centered"
     )
+
+    load_css(os.path.join(os.getcwd(), "static", "style.css"))
+    inject_local_font(os.path.join(os.getcwd(), "static", "AdobeClean.otf"), "AdobeClean")    
 
     if not render_login_wall():
         return 
